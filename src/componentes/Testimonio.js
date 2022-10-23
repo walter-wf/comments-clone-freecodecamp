0a2531d027cb97b98d3022c1ffa00f2421804c7d
Walter Franz <walter.franz@gmail.com>
2022-10-23 19:03:58 -0300

    Final

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -6,11 +6,17 @@ function Testimonio(props) {
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require(`../imagenes/testimonio-${props.imagen}.JPG`)} 
-        alt="Foto de Emma"/>
+        src={require(`../imagenes/testimonio-${props.imagen}.JPG`)}
+        alt="Foto de Emma"
+      />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"> <strong>{props.nombre}</strong>en <strong>{props.pais}</strong></p>
-        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="nombre-testimonio">
+          {" "}
+          <strong>{props.nombre}</strong>en <strong>{props.pais}</strong>
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
         <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
